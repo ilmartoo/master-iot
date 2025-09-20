63071246bb79fafc2278bd7316d54966bc287eb2
ilmartoo <martinszga@gmail.com>
2025-09-20 03:23:22 +0200

    Updated practica 1

--- Sistemas Empotrados/Practica 1/Tarea 2/main/main.c
+++ Sistemas Empotrados/Practica 1/Tarea 2/main/main.c
@@ -1,41 +1,38 @@
 #include <stdio.h>
+
+#include "driver/adc.h"
+#include "driver/adc_types_legacy.h"
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
-#include "driver/adc_types_legacy.h"
-#include "driver/adc.h"
 
 #define msToTicks(ms) (ms / portTICK_PERIOD_MS)
 
-#define LDR_CHANNEL ADC_CHANNEL_7 // LDR analog channel (pin)
+#define LDR_CHANNEL ADC_CHANNEL_7  // LDR analog channel (pin)
 
-#define ANALOG_ATTENUATION ADC_ATTEN_DB_12 // Atenuation for the analog value
-#define ANALOG_RESOLUTION 12               // Resolution for the analog value
+#define ANALOG_ATTENUATION ADC_ATTEN_DB_12  // Atenuation for the analog value
+#define ANALOG_RESOLUTION  12               // Resolution for the analog value
 
-#define LOOP_WAIT_TIME 2000 // Wait time in milliseconds
+#define LOOP_WAIT_TIME 2000  // Wait time in milliseconds
 
-void config_ldr(adc1_channel_t channel);         // Config channel as LDR input
-void execute_task_2(adc1_channel_t ldr_channel); // Execute the desired task
+void config_ldr(adc1_channel_t channel);          // Config channel as LDR input
+void execute_task_2(adc1_channel_t ldr_channel);  // Execute the desired task
 
-void app_main(void)
-{
+void app_main(void) {
     config_ldr(LDR_CHANNEL);
 
-    while (1)
-    {
+    while (1) {
         execute_task_2(LDR_CHANNEL);
         vTaskDelay(msToTicks(LOOP_WAIT_TIME));
     }
 }
 
-void config_ldr(adc1_channel_t channel)
-{
+void config_ldr(adc1_channel_t channel) {
     adc1_config_channel_atten(channel, ANALOG_ATTENUATION);
     adc1_config_width(ANALOG_RESOLUTION);
 }
 
-void execute_task_2(adc1_channel_t ldr_channel)
-{
+void execute_task_2(adc1_channel_t ldr_channel) {
     int ldr_value = adc1_get_raw(ldr_channel);
     printf("LDR value = %d\n", ldr_value);
 }
